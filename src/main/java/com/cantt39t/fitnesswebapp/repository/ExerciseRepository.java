15b0f38789f7ee821beeb6dc56637e339af7eeec
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:13 +0300

    Update ExerciseRepository.java

--- src/main/java/com/cantt39t/fitnesswebapp/repository/ExerciseRepository.java
+++ src/main/java/com/cantt39t/fitnesswebapp/repository/ExerciseRepository.java
@@ -4,9 +4,11 @@ import com.cantt39t.fitnesswebapp.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
 
-
+    List<Exercise> findAllByTrainingsId(Integer trainingId);
 
 }
