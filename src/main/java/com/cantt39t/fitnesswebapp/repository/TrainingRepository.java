958b1e2f62c5bfb9fe516727926ecd7b97e81c35
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:15 +0300

    Update TrainingRepository.java

--- src/main/java/com/cantt39t/fitnesswebapp/repository/TrainingRepository.java
+++ src/main/java/com/cantt39t/fitnesswebapp/repository/TrainingRepository.java
@@ -4,6 +4,15 @@ import com.cantt39t.fitnesswebapp.model.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Repository
 public interface TrainingRepository extends JpaRepository<Training, Integer> {
+
+    List<Training> findAllByComplexityAndNameContainingIgnoreCase(Training.@NotNull Complexity complexity, @NotNull String name);
+
+    List<Training> findAllByNameContainingIgnoreCase(@NotNull String name);
+
+    List<Training> findAllByComplexity(Training.@NotNull Complexity complexity);
 }
