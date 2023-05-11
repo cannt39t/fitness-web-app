68707d70e4fa5912f4ddad3e72675635ea60569b
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:18 +0300

    Update CommentService.java

--- src/main/java/com/cantt39t/fitnesswebapp/service/CommentService.java
+++ src/main/java/com/cantt39t/fitnesswebapp/service/CommentService.java
@@ -1,4 +1,14 @@
 package com.cantt39t.fitnesswebapp.service;
 
+import com.cantt39t.fitnesswebapp.dto.CreateCommentDto;
+import com.cantt39t.fitnesswebapp.model.Comment;
+
+import java.util.List;
+
 public interface CommentService {
+
+    List<Comment> getCommentsForTraining(Integer trainingId);
+
+    void addCommentToTraining(CreateCommentDto createCommentDto);
+
 }
