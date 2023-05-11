82207de3862d183d2616d575d023ea02866b497b
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:37 +0300

    Update TrainingService.java

--- src/main/java/com/cantt39t/fitnesswebapp/service/TrainingService.java
+++ src/main/java/com/cantt39t/fitnesswebapp/service/TrainingService.java
@@ -1,4 +1,19 @@
 package com.cantt39t.fitnesswebapp.service;
 
+import com.cantt39t.fitnesswebapp.model.Training;
+
+import java.util.List;
+
 public interface TrainingService {
+
+    List<Training> getAllTrainings();
+
+    Training getTrainingById(Integer id);
+
+    List<Training> getTrainingsByDifficultyAndName(String difficulty, String searchQuery);
+
+    List<Training> getTrainingsByName(String searchQuery);
+
+    List<Training> getTrainingsByDifficulty(String difficulty);
+
 }
