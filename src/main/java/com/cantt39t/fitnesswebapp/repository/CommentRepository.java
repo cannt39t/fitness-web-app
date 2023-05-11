fb5cb862beedbc36132c1078727b280a666b4027
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:11 +0300

    Update CommentRepository.java

--- src/main/java/com/cantt39t/fitnesswebapp/repository/CommentRepository.java
+++ src/main/java/com/cantt39t/fitnesswebapp/repository/CommentRepository.java
@@ -5,9 +5,11 @@ import com.cantt39t.fitnesswebapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-
+    List<Comment> findAllByTrainingId(Integer id);
 
 }
