671ff8a467152270029ed37b0d4a857d25f64fe5
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-11 18:37:24 +0300

    Update ExerciseService.java

--- src/main/java/com/cantt39t/fitnesswebapp/service/ExerciseService.java
+++ src/main/java/com/cantt39t/fitnesswebapp/service/ExerciseService.java
@@ -1,4 +1,11 @@
 package com.cantt39t.fitnesswebapp.service;
 
+import com.cantt39t.fitnesswebapp.model.Exercise;
+
+import java.util.List;
+
 public interface ExerciseService {
+
+    List<Exercise> getExercisesForTraining(Integer id);
+
 }
